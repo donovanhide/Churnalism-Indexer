59f6733c8050c3c267ad0204967fd3ef53758f15
Donovan Hide <donny@sur.ly>
2010-12-16 04:36:29 +0000

    debug

--- node-churn/src/server.js
+++ node-churn/src/server.js
@@ -17,6 +17,7 @@ function main(app){
 		res.end(body, 'utf8');
 	});
 	app.post('/search',function(req,res){
+	    console.log(req.body.text);
 		churnIndex.searchString(req.body.text,20,(req.body.text.length/200),function(results){
 			var json = JSON.stringify(results);
 			res.writeHead(200, {
